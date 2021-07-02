26fa5408d8fbb20c5a597cb2f393c6be73033135
Valentin Panov <valentin.panov@me.com>
2021-07-02 06:45:54 +0300

    SW cleared, offline isnt work

--- src/js/engine/service.worker.js
+++ src/js/engine/service.worker.js
@@ -5,39 +5,24 @@ const version = 'v1';
 const cacheName = `ahj-${version}`;
 const files = ['./', './index.html', './main.js', './main.css'];
 
-async function removeOldCache(retain) {
-  const keys = await caches.keys();
-  return Promise.all(keys.filter((key) => !retain.includes(key)).map((key) => caches.delete(key)));
-}
-
-self.addEventListener('install', async (event) => {
+self.addEventListener('install', (event) => {
   console.log('SW Installed');
-  event.waitUntil(async () => {
-    const cache = await caches.open(cacheName);
-    await cache.addAll(files);
-    await self.skipWaiting();
+  event.waitUntil(() => {
+    caches
+      .open(cacheName)
+      .then((cache) => cache.addAll(files))
+      .then(self.skipWaiting());
   });
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', () => {
   console.log('SW activated');
-  event.waitUntil(async () => {
-    await removeOldCache([cacheName]);
-    await self.clients.claim();
-  });
 });
 
 self.addEventListener('fetch', (event) => {
   console.log('SW fetchin:', event.request.url);
 
-  event.respondWith(async () => {
-    const cache = await caches.open(cacheName);
-
-    const cachedResponse = await cache.match(event.request);
-
-    if (cachedResponse) {
-      return cachedResponse;
-    }
-    return fetch(event.request);
-  });
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
 });
